b5372b2856a5d68059b902b14f0ef4fb672548a6
Anjali Sinha <104860990+Anjali270391@users.noreply.github.com>
2022-05-06 17:31:00 +0530

    Update streamlit_app.py
    Editing the doc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 
 streamlit.title('My Parents new healthy Diner')
-streamlit.text("My Mom\'s new healthy Diner \n Breakfast Menu \n Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg")
+streamlit.header('Breakfast Menu') \n Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg")
+streamlit.text("Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg")

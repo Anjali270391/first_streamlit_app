7a2cebd3c78b2339ec060b4f1f34e5a39c2fd4b6
Anjali Sinha <104860990+Anjali270391@users.noreply.github.com>
2022-05-06 17:19:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit
 
 streamlit.title('My Parents new healthy Diner')
-streamlit.title("My Mom\'s new healthy Diner \n Breakfast Menu \n
-               Omega 3 & Blueberry Oatmeal \n
-               Kale, Spinach & Rocket Smoothie \n
-               Hard-Boiled Free-Range Egg")
+streamlit.title("My Mom\'s new healthy Diner \n Breakfast Menu \n Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg")
